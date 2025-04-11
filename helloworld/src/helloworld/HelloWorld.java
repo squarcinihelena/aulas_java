c474345d93e8c834c0db069dd57980267eec6113
Helena <maria.helen@live.com>
2025-04-11 11:15:51 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		
+       System.out.print("Hello world!\n"); //pula linha esse \n 
+       System.out.print("A turma Java 82 é top!");
+       
+      
+       
+	}
+
+}
